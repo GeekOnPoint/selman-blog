455eb0310a20e40b653606fdbea18bd69a16a8ae
Andy Gonzalez <24887628+zelaznogydna91@users.noreply.github.com>
2023-08-24 20:04:43 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }) {
       <Analytics />
       <LayoutWrapper>
         // <Component {...pageProps} />
-        <div>Hello<div>
+        <div>Hello</div>
       </LayoutWrapper>
     </ThemeProvider>
   )
